4cc7e42da9f77d190bf14c91af22c802861b085c
31jay <jayadevtripathi@gmail.com>
2024-06-28 20:24:58 +0545

    updated

--- StreamlitPython/EduMain.py
+++ StreamlitPython/EduMain.py
@@ -1,13 +1,23 @@
 import streamlit as st
 import base64
 
+# Ensure userStatus is set in session state
 if 'userStatus' not in st.session_state:
-    st.session_state.userStatus=False 
+    st.session_state.userStatus = False
 
-def setStatus():
-    st.session_state.userStatus=True 
+if 'statusAction' not in st.session_state:
+    st.session_state.statusAction = None 
+
+# def setStatusTrue():
+#     st.session_state.userStatus = True
+
+def login_clicked():
+    st.session_state.userStatus=True
+    st.session_state.statusAction='Login'
     
-
+def signup_clicked():
+    st.session_state.userStatus=True
+    st.session_state.statusAction='SignUp'
 
 def preHome_UI():
     with open("assets/edumap.png", "rb") as image_file:
@@ -23,30 +33,35 @@ def preHome_UI():
 
     col1, col2, col3, col4 = st.columns([2, 2, 2, 2])
 
-    if col2.button('Login', use_container_width=True,on_click=setStatus):
-        return 'Login'
-    if col3.button('SignUp', use_container_width=True, on_click=setStatus):
-        return 'SignUp'
-
+    col2.button('Login', use_container_width=True, on_click=login_clicked)
+        # st.session_state.statusAction='Login'
+        # st.write('Login Clicked')
+    col3.button('SignUp', use_container_width=True, on_click=signup_clicked)
+        # st.session_state.statusAction='SignUp'
+        # st.write('SignUp Clicked ')
 
 def signUp():
-    with st.form('SignUp Form',clear_on_submit=False,border=True):
-        st.write('''<p style='margin-bottom: 5px; font-size: 16px; font-family: sans-serif; color: green; font-weight: bold'> Login</p>''', unsafe_allow_html=True)
-        st.form_submit_button('Register')
-
+    col1,col2,col3=st.columns([1,5,1])
+    with col2.form('SignUp Form', clear_on_submit=False,border=True):
+        st.write('''<p style='margin-bottom: 5px; font-size: 16px; font-family: sans-serif; color: green; font-weight: bold'> Sign Up</p>''', unsafe_allow_html=True)
+        fullName=st.text_input('Full Name ')
+        email=st.text_input('Email ')
+        password=st.text_input('Password',type='password')
+        district=st.text_input('District')
+        area=st.text_input('Area')
+        qualification=st.text_area('Qualifications',height=10)
+        if st.form_submit_button('Register',use_container_width=True):
+            pass 
 
 def Login():
-    pass 
+    st.write('''<p style='margin-bottom: 5px; font-size: 16px; font-family: sans-serif; color: green; font-weight: bold'> Login</p>''', unsafe_allow_html=True)
+    # Add login form inputs here (e.g., username, password, etc.)
+    st.form_submit_button('Login')
 
-
-if __name__=='__main__':
-    
-    status_action=None
-    if st.session_state.userStatus==False:
-        status_action=preHome_UI()
-    if status_action=='Login':
+if __name__ == '__main__':
+    if not st.session_state.userStatus:
+        preHome_UI()
+    if st.session_state.statusAction == 'Login':
         Login()
-    if status_action=='SignUp':
+    if st.session_state.statusAction == 'SignUp':
         signUp()
-        
-    
