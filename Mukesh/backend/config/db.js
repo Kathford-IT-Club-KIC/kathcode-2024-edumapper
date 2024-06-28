2443c2b7faa71e61ff9290ba665528c842ea28b6
BakaYuki <mukeshpr443@gmail.com>
2024-06-28 12:54:53 +0545

    back

---
+++ Mukesh/backend/config/db.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose"
+
+ export const connectDB = async () => {
+    await mongoose.connect('mongodb+srv://mukeshpr443:mukesh@cluster0.4ycn4qv.mongodb.net/?appName=Cluster0').then(() => console.log("DB connected"));
+}
+
