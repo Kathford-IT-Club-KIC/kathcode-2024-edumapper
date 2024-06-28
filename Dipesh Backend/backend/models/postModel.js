dd3ca67db9c82bf3c7c6d2c7f7da8e2c867d3954
31jay <jayadevtripathi@gmail.com>
2024-06-28 14:34:12 +0545

    FilterBydate

--- Dipesh Backend/backend/models/postModel.js
+++ Dipesh Backend/backend/models/postModel.js
@@ -2,13 +2,12 @@ import mongoose from "mongoose"
 
 const postSchema = new mongoose.Schema({
     name: { type: String },
-    description: { type: String,required:true },
-    image: { type: String, required: true},
-    // {timestamp:true}
-    
-})
+    description: { type: String, required: true },
+    image: { type: String, required: true },
 
-const postModel=mongoose.models.post || mongoose.model("post",postSchema)
+}, { timestamp: true })
+
+const postModel = mongoose.models.post || mongoose.model("post", postSchema)
 
 export default postModel;
 
